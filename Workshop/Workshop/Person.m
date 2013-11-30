7ed00c07a88d7fbff9ffcf1dee6bc1a238121759
Hugo Tunius <hugo.tunius@gmail.com>
2013-11-30 21:26:30 +0100

    Add correctly named models

---
+++ Workshop/Workshop/Person.m
@@ -0,0 +1,19 @@
+//
+//  Person.m
+//  Workshop
+//
+//  Created by Hugo Tunius on 30/11/13.
+//  Copyright (c) 2013 Hugo Tunius. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@implementation Person
+
+@dynamic dateOfBirth;
+@dynamic firstName;
+@dynamic lastName;
+@dynamic phoneNumber;
+
+@end

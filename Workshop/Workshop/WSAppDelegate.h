9042412cc007fb2b9cce7dbbc26cc2ab62677433
Hugo Tunius <hugo.tunius@gmail.com>
2013-11-30 21:14:33 +0100

    Project created

---
+++ Workshop/Workshop/WSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  WSAppDelegate.h
+//  Workshop
+//
+//  Created by Hugo Tunius on 30/11/13.
+//  Copyright (c) 2013 Hugo Tunius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Workshop/Workshop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Workshop
+//
+//  Created by Hugo Tunius on 30/11/13.
+//  Copyright (c) 2013 Hugo Tunius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
